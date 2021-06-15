e9ccf89297e2fa54978c7c55d665dc84a963d6ed
arbazkhan123-s <83135990+arbazkhan123-s@users.noreply.github.com>
2021-06-15 22:24:37 +0530

    commit updates
    yes welcome

--- index.js
+++ index.js
@@ -8,8 +8,37 @@ function off() {
 }
 window.addEventListener("offline", off)
 
+console.log('hello world');
 
 
+let input = document.getElementById('name')
+console.log(input)
+
+input.addEventListener('blur',()=>{
+    let str = input.value
+    let regex = /^[a-zA-Z]([\s0-9a-zA-Z]){2,15}$/;
+    console.log(str,regex);
+    if (regex.test(str)) {
+        input.classList.remove('is-invalid')
+        
+    }else{
+        input.classList.add('is-invalid')
+    }
+})
+
+let email = document.getElementById('email');
+
+email.addEventListener('blur',()=>{
+    let emailstr = email.value;
+    let  regex = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@{[a-zA-Z0-9_\-\.]+0\.([a-zA-Z]{2,5}){1,25})+)*$/;
+
+    if (regex.test(emailstr)) {
+        email.classList.remove('is-invalid')
+    }else{
+        email.classList.add('is-invalid')
+    }
+
+})
 
 
 
